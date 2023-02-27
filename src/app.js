7c28c8468f25f3e25a1d658d2a30e890835f4b50
Tim <39254058+sunjaeyoon@users.noreply.github.com>
2023-02-27 13:49:45 -0500

    working

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 /*
 Followed this guy's instructions: https://www.youtube.com/watch?v=8K5wJeVgjrM
-*/ 
+*/
 
 // LIBRARIES
 import * as THREE from 'three';
@@ -14,18 +14,18 @@ import vertex from './shaders/vertex.glsl';
 import fragment from './shaders/fragment.glsl';
 
 
-export default class Sketch{
-    constructor(){
+export default class Sketch {
+    constructor() {
         // Scene, Camera, Renderer
         this.scene = new THREE.Scene();
-        this.camera = new THREE.PerspectiveCamera( 70, (window.innerWidth/2) / window.innerHeight, 0.1, 1000 );
-        this.camera.position.set(-2,2,-2);
+        this.camera = new THREE.PerspectiveCamera(70, (window.innerWidth / 2) / window.innerHeight, 0.1, 1000);
+        this.camera.position.set(-2, 2, -2);
         this.renderer = new THREE.WebGLRenderer({
             canvas: document.querySelector('#bg'),
         });
-        this.renderer.setSize( window.innerWidth/2, window.innerHeight );
+        this.renderer.setSize(window.innerWidth / 2, window.innerHeight);
         this.time = 0;
-        
+
         // Orbit Controls
         this.controls = new OrbitControls(this.camera, this.renderer.domElement)
         this.createGUI();
@@ -39,7 +39,9 @@ export default class Sketch{
         this.arrows = [];
         this.arrows_dir = [];
         this.offset = [];
-        
+        this.track_mag_x = [];
+        this.track_max = window.innerWidth / 2;
+
         // Add Objects
         //this.createMesh();
         this.createGrid();
@@ -50,21 +52,22 @@ export default class Sketch{
         this.animate();
     };
 
-    onWindowResize = () =>{
-        this.camera.aspect = window.innerWidth / window.innerHeight;
+    onWindowResize = () => {
+        this.camera.aspect = (window.innerWidth / 2) / window.innerHeight;
         this.camera.updateProjectionMatrix();
-        this.renderer.setSize(window.innerWidth/2, window.innerHeight);
+        this.renderer.setSize(window.innerWidth / 2, window.innerHeight);
     };
 
-    render = () =>{
+    render = () => {
         this.time++;
-        this.renderer.render( this.scene, this.camera );
+        this.renderer.render(this.scene, this.camera);
     };
 
-    animate = () =>{
+    animate = () => {
         //console.log(this.arrows.dir);
         //console.log(this.arrows.position)
-        this.arrows.forEach( (element,index) => {
+        let sum = 0;
+        this.arrows.forEach((element, index) => {
             //this.arrows_dir[index] 
             let angle = this.offset[index];
             let current_pos = this.arrows_dir[index];
@@ -73,36 +76,42 @@ export default class Sketch{
             let new_pos = this.multiplyMatrixAndPoint(rot_mat, current_pos);
             this.arrows_dir[index] = new_pos;
             element.setDirection(new_pos);
-        }) 
+            //console.log(e)
+            sum += new_pos.x;
+        })
+        //console.log(Math.abs(sum)/11)
+        // Update Tracker
+        this.track_mag_x.unshift(sum);
+        this.track_mag_x.length = Math.min(this.track_mag_x.length, this.track_max);
 
-        if(this.time == 150 && this.time > 0){
+        //console.log(this.track_mag_x)
+        if (this.time == 150 && this.time > 0) {
             this.refocusPulse();
-        }else if(this.time % 600 == 0){
-            this.refocusPulse();
-            //alert("Refocused!!!")
+        } else if (this.time % 300 == 150 && this.time > 0) {
+            this.refocusPulseVFA();
         }
 
-        this.render();  
-        requestAnimationFrame( this.animate );
-        
+        this.render();
+        requestAnimationFrame(this.animate);
+
         this.updateGraph(); //Plotlyjs
     };
 
     // Manual Rotation Matrices
     rotationMatrix = (axis, angle) => {
-        if(axis === 'x'){
-            return [1, 0, 0, 
-                    0, Math.cos(angle), -Math.sin(angle),
-                    0, Math.sin(angle), Math.cos(angle)]
-        }else if(axis === 'z'){
+        if (axis === 'x') {
+            return [1, 0, 0,
+                0, Math.cos(angle), -Math.sin(angle),
+                0, Math.sin(angle), Math.cos(angle)]
+        } else if (axis === 'z') {
             return [Math.cos(angle), 0, Math.sin(angle),
-                    0            , 1, 0,
-                    -Math.sin(angle), 0, Math.cos(angle)]
-        }else if(axis === 'y'){
+                0, 1, 0,
+            -Math.sin(angle), 0, Math.cos(angle)]
+        } else if (axis === 'y') {
             return [Math.cos(angle), -Math.sin(angle), 0,
-                    Math.sin(angle),  Math.cos(angle), 0,
-                    0, 0 ,1]
-        }else{
+            Math.sin(angle), Math.cos(angle), 0,
+                0, 0, 1]
+        } else {
             return None
         }
     };
@@ -113,11 +122,11 @@ export default class Sketch{
         let c0r0 = matrix[0],
             c1r0 = matrix[1],
             c2r0 = matrix[2];
-            
+
         let c0r1 = matrix[3],
             c1r1 = matrix[4],
             c2r1 = matrix[5];
-            
+
         let c0r2 = matrix[6],
             c1r2 = matrix[7],
             c2r2 = matrix[8];
@@ -140,61 +149,21 @@ export default class Sketch{
     };
 
     createGraph = () => {
-        /*
-        let layout = {
 
-            title:'Signal Intensity',
-            //margin: { t: 0 } 
-        };
-
-        let trace1 = {
-
-            x: [1, 2, 3, 4],
-          
-            y: [10, 15, 13, 17],
-          
-            mode: 'markers'
-          
-        };
-
-        var data = [trace1];
-        
-        Plotly.newPlot( this.TESTER, data, layout)
-
-        setInterval(function() {
-            Plotly.extendTraces('tester', { y: [[Math.random()]] }, [0]);
-        }
-          , 200);*/
-            
         var canvas = document.getElementById('DemoCanvas');
-        canvas.style.width ='100%';
-        canvas.style.height='100%';
+        canvas.style.width = '100%';
+        canvas.style.height = '100%';
         // ...then set the internal size to match
-        canvas.width  = canvas.offsetWidth;
+        canvas.width = canvas.offsetWidth;
         canvas.height = canvas.offsetHeight;
-        
-        //Always check for properties and methods, to make sure your code doesn't break in other browsers.
-        if (canvas.getContext) 
-        {
-            var context = canvas.getContext('2d');
-            // Reset the current path
-            context.beginPath(); 
-            // Staring point (10,45)
-            context.moveTo(0, canvas.height/2);
-            // End point (180,47)
-            context.lineTo(canvas.width, canvas.height/2);
-            // Make the line visible
-            context.stroke();
-        }
-
-
     }
 
     updateGraph = () => {
-
-        //const canvas = document.getElementById('DemoCanvas');;
-        //const ctx = canvas.getContext("2d");
-        function plotSine(ctx, xOffset, yOffset) {
+        function plotData(ctx, xOffset, yOffset, maximum, data) {
+            // Write
+            ctx.font = "30px Arial";
+            ctx.fillText("Sum of Phase in X direction", 10,50);
+            
             var width = ctx.canvas.width;
             var height = ctx.canvas.height;
             var scale = 20;
@@ -205,56 +174,56 @@ export default class Sketch{
 
             // console.log("Drawing point...");
             // drawPoint(ctx, yOffset+step);
-            
+
             var x = 4;
             var y = 0;
             var amplitude = 40;
             var frequency = 20;
-            ctx.moveTo(width/2, height/2);
-            while (x < width) {
-                y = height/2 + amplitude * Math.sin((x+xOffset)/frequency);
-                ctx.lineTo(x, y);
+            ctx.moveTo(width / 2, height / 2);
+            while (x < maximum) {
+                y = height / 2 + data[x]*100;
+                ctx.lineTo(-(x-width-4) / 2, y);
                 x++;
                 // console.log("x="+x+" y="+y);
             }
             ctx.stroke();
             ctx.save();
 
-            //console.log("Drawing point at y=" + y);
-            //drawPoint(ctx, y);
+            // console.log("Drawing point at y=" + y);
+            // drawPoint(ctx, y);
             ctx.stroke();
             ctx.restore();
         }
 
-        function showAxes(ctx,axes) {
+        function showAxes(ctx, axes) {
             var width = ctx.canvas.width;
             var height = ctx.canvas.height;
             var xMin = 0;
-            
+
             ctx.beginPath();
             ctx.strokeStyle = "rgb(128,128,128)";
-            
+
             // X-Axis
-            ctx.moveTo(xMin, height/2);
-            ctx.lineTo(width, height/2);
-            
+            ctx.moveTo(xMin, height / 2);
+            ctx.lineTo(width, height / 2);
+
             // Y-Axis
-            ctx.moveTo(width/2, 0);
-            ctx.lineTo(width/2, height);
+            ctx.moveTo(width / 2, 0);
+            ctx.lineTo(width / 2, height);
 
             // Starting line
             ctx.moveTo(0, 0);
             ctx.lineTo(0, height);
-            
+
             ctx.stroke();
         }
 
-        function drawPoint(ctx, y) {            
+        function drawPoint(ctx, y) {
             var radius = 3;
-            
+
             ctx.beginPath();
-            
-            ctx.moveTo(ctx.width, ctx.height/2);
+
+            ctx.moveTo(ctx.width, ctx.height / 2);
             // Hold x constant at 4 so the point only moves up and down.
             ctx.arc(4, y, radius, 0, 2 * Math.PI, false);
 
@@ -263,18 +232,18 @@ export default class Sketch{
             ctx.lineWidth = 1;
             ctx.stroke();
         }
-        
+
         let canvas = document.getElementById('DemoCanvas');
         if (canvas.getContext) {
             let context = canvas.getContext("2d");
-        
+
             context.clearRect(0, 0, canvas.width, canvas.height);
             showAxes(context);
-            context.save();            
-            
-            plotSine(context, this.time, 50);
+            context.save();
+
+            plotData(context, this.time, 50, this.track_max, this.track_mag_x);
             context.restore();
-     
+
         }
 
     }
@@ -283,10 +252,10 @@ export default class Sketch{
         // Reset Animation
         this.gui = new GUI();
         this.gui.domElement.id = 'gui';
-        this.resetAnimation = 
+        this.resetAnimation =
             () => {
-                this.arrows.forEach( (element,index) => {
-                    const dir = new THREE.Vector3( 0, 0, 1 );
+                this.arrows.forEach((element, index) => {
+                    const dir = new THREE.Vector3(0, 0, 1);
                     this.arrows_dir[index] = dir;
                     element.setDirection(dir);
                 });
@@ -294,57 +263,66 @@ export default class Sketch{
                 this.time = 0;
             };
 
-        this.gui.add(this,'resetAnimation').name('Reset Animation');
+        this.gui.add(this, 'resetAnimation').name('Reset Animation');
 
         // Refocusing
-        this.refocusPulse = 
+        this.refocusPulse =
             () => {
-                this.arrows.forEach( (element,index) => {
+                this.arrows.forEach((element, index) => {
                     let angle = Math.PI;
                     let current_pos = this.arrows_dir[index];
                     let rot_mat = this.rotationMatrix('x', angle);
-        
+
                     let new_pos = this.multiplyMatrixAndPoint(rot_mat, current_pos);
                     this.arrows_dir[index] = new_pos;
                     element.setDirection(new_pos);
                 });
             };
 
-        this.gui.add(this,'refocusPulse').name('180 RF Pulse');
+        this.gui.add(this, 'refocusPulse').name('180 RF Pulse');
 
         // Refocusing VFA
-        this.refocusPulseVFA = 
-              function() {
+        this.refocusPulseVFA =
+            function () {
                 //console.log(this.rotationMatrix('x', 90))
-                this.arrows.forEach( (element,index) => {
-                    let angle = 90*Math.PI/180;
+                this.arrows.forEach((element, index) => {
+                    let angle = 90 * Math.PI / 180;
                     let current_pos = this.arrows_dir[index];
                     let rot_mat = this.rotationMatrix('x', angle);
-        
+
                     let new_pos = this.multiplyMatrixAndPoint(rot_mat, current_pos);
                     this.arrows_dir[index] = new_pos;
                     element.setDirection(new_pos);
                 });
             };
 
-        this.gui.add(this,'refocusPulseVFA').name('VFA RF Pulse');
+        this.gui.add(this, 'refocusPulseVFA').name('refocusPulseVFA');
+
+        // Refocusing VFA
+        this.testbutton =
+            function () {
+                console.log(this.track_mag_x)
+                this.updateGraph();
+            };
+
+        this.gui.add(this, 'testbutton').name('testbutton');
     }
 
-    createMesh = () =>{
+    createMesh = () => {
         //Mesh
         let number = 512;
         this.geometry = new THREE.BufferGeometry();
-        this.positions = new THREE.BufferAttribute(new Float32Array(number*number*3),3);
-        this.coordinates = new THREE.BufferAttribute(new Float32Array(number*number*3),3);
+        this.positions = new THREE.BufferAttribute(new Float32Array(number * number * 3), 3);
+        this.coordinates = new THREE.BufferAttribute(new Float32Array(number * number * 3), 3);
 
         let index = 0;
         for (let i = 0; i < number; i++) {
-            let posX = i - number/2;
+            let posX = i - number / 2;
             for (let j = 0; j < number; j++) {
-                this.positions.setXYZ(index, posX, j-number/2, 0);
+                this.positions.setXYZ(index, posX, j - number / 2, 0);
                 this.coordinates.setXYZ(index, i, j, 0);
                 index++;
-            }    
+            }
         }
 
         this.geometry.setAttribute('position', this.positions);
@@ -360,47 +338,47 @@ export default class Sketch{
 
     createGrid = () => {
         this.gridHelper = new THREE.GridHelper();
-        this.scene.add( this.gridHelper );
+        this.scene.add(this.gridHelper);
         this.gridHelper2 = new THREE.GridHelper();
-        this.axesHelper = new THREE.AxesHelper( 5 );
-        this.scene.add( this.axesHelper );
-    } 
+        this.axesHelper = new THREE.AxesHelper(5);
+        this.scene.add(this.axesHelper);
+    }
 
     createArrow = () => {
-
-        for (let index = 0; index < 11; index++) {
-            const dir = new THREE.Vector3( 0, 0, 1 );
+        let total = 180
+        for (let index = 0; index < total; index++) {
+            const dir = new THREE.Vector3(0, 0, 1);
 
             //normalize the direction vector (convert to vector of length 1)
             dir.normalize();
 
-            const origin = new THREE.Vector3( 0, 0, 0 );
+            const origin = new THREE.Vector3(0, 0, 0);
             const length = 2;
 
             var letters = "0123456789ABCDEF";
-      
+
             // html color code starts with #
             var color = '#';
-            
+
             // generating 6 times as HTML color code consist
             // of 6 letter or digits
             for (var i = 0; i < 6; i++)
-            color += letters[(Math.floor(Math.random() * 16))];
-            
+                color += letters[(Math.floor(Math.random() * 16))];
+
             //console.log(color);
             const hex = color;
 
-            const arrowHelper = new THREE.ArrowHelper( dir, origin, length, hex );
-            this.arrows.push( arrowHelper );
-            this.arrows_dir.push( dir );
-            this.offset.push( (index-5)/3000); 
-            this.scene.add( arrowHelper );
+            const arrowHelper = new THREE.ArrowHelper(dir, origin, length, hex);
+            this.arrows.push(arrowHelper);
+            this.arrows_dir.push(dir);
+            this.offset.push((index - Math.floor(total/2)) / 30000);
+            this.scene.add(arrowHelper);
         }
-        
+
     }
 
-    
-   
+
+
 }
 
 var draw = new Sketch();
